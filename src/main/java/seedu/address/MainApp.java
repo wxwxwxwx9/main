b5631800a3bab9ed73aac88644f1e700cb4604b8
chifa3 <foochifa@gmail.com>
2020-03-11 00:19:08 +0800

    Add empty diary stub for MainApp to work with ModelManager

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -21,6 +21,7 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.InternshipDiary;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
 import seedu.address.storage.JsonAddressBookStorage;
@@ -90,7 +91,8 @@ public class MainApp extends Application {
             initialData = new AddressBook();
         }
 
-        return new ModelManager(initialData, userPrefs);
+        //Stub for ModelManager to work | replace first parameter with initialData after refactoring
+        return new ModelManager(new InternshipDiary(), userPrefs);
     }
 
     private void initLogging(Config config) {
