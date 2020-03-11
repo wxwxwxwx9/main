da546ee4a62da7ebdea4cbb8124358bc468b7ed2
chifa3 <foochifa@gmail.com>
2020-03-11 18:17:37 +0800

    Refactor all commands except EditCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,60 +1,63 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
+import seedu.address.model.internship.InternshipApplication;
 
 /**
- * Adds a person to the address book.
+ * Adds a person to the internship diary.
  */
 public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds an internship application to the internship diary. "
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
+            + PREFIX_COMPANY + "COMPANY "
+            + PREFIX_ROLE + "ROLE "
+            + PREFIX_ADDRESS + "ADDRESS "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + PREFIX_DATE + "APPLICATION DATE "
+            + PREFIX_PRIORITY + "PRIORITY "
+            + PREFIX_STATUS + "STATUS "
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_COMPANY + "Google "
+            + PREFIX_ROLE + "Software Engineer "
+            + PREFIX_ADDRESS + "123 Kent Ridge Road "
+            + PREFIX_PHONE + "67654321 "
+            + PREFIX_EMAIL + "example@google.com "
+            + PREFIX_DATE + "10 Feb 2020 "
+            + PREFIX_PRIORITY + "5 "
+            + PREFIX_STATUS + "Active ";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New internship application added: %1$s";
+    public static final String MESSAGE_DUPLICATE_INTERNSHIP =
+            "This internship application already exists in the internship diary";
 
-    private final Person toAdd;
+    private final InternshipApplication toAdd;
 
     /**
-     * Creates an AddCommand to add the specified {@code Person}
+     * Creates an AddCommand to add the specified {@code internshipApplication}
      */
-    public AddCommand(Person person) {
-        requireNonNull(person);
-        toAdd = person;
+    public AddCommand(InternshipApplication internshipApplication) {
+        requireNonNull(internshipApplication);
+        toAdd = internshipApplication;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasPerson(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        if (model.hasInternshipApplication(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_INTERNSHIP);
         }
 
-        model.addPerson(toAdd);
+        model.addInternshipApplication(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 
