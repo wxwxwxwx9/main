842a5adeab21e635f8a71ac0927b77dabbf41844
chifa3 <foochifa@gmail.com>
2020-03-12 12:33:05 +0800

    Remove validStatuses with whitespace

--- src/main/java/seedu/address/model/status/Status.java
+++ src/main/java/seedu/address/model/status/Status.java
@@ -7,9 +7,8 @@ public enum Status {
     ACTIVE, INACTIVE, PLAN_TO_APPLY, APPLICATION_DONE, INTERVIEW_SCHEDULED, INTERVIEW_DONE, ACCEPTED, REJECTED;
 
     private static final String[] validStatuses = new String[] {
-            "active", "inactive", "plan to apply", "plan_to_apply", "application done", "application_done",
-            "interview scheduled", "interview_scheduled", "interview done", "interview_done", "accepted",
-            "rejected"
+            "active", "inactive", "plan_to_apply", "application_done",
+            "interview_scheduled", "interview_done", "accepted", "rejected"
     };
     public static final String MESSAGE_CONSTRAINTS =
             "Status should only be one of the following strings: "
