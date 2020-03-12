d555b84e20d386aff3196502d1cf6c252ea3427b
chifa3 <foochifa@gmail.com>
2020-03-12 09:16:41 +0800

    Add new methods in Status.java

--- src/main/java/seedu/address/model/status/Status.java
+++ src/main/java/seedu/address/model/status/Status.java
@@ -5,4 +5,27 @@ package seedu.address.model.status;
  */
 public enum Status {
     ACTIVE, INACTIVE, PLAN_TO_APPLY, APPLICATION_DONE, INTERVIEW_SCHEDULED, INTERVIEW_DONE, ACCEPTED, REJECTED;
+
+    private static final String[] validStatuses = new String[] {
+            "active", "inactive", "plan to apply", "plan_to_apply", "application done", "application_done",
+            "interview scheduled", "interview_scheduled", "interview done", "interview_done", "accepted",
+            "rejected"
+    };
+    public static final String MESSAGE_CONSTRAINTS =
+            "Status should only be one of the following strings: "
+            + "active, inactive, plan to apply, plan_to_apply, application done, application_done"
+            + "interview scheduled, interview_scheduled, interview done, interview_done, accepted"
+            + "rejected";
+
+    /**
+     * Returns true is given string is a valid status.
+     */
+    public static boolean isValidStatus(String test) {
+        String status = test.toLowerCase();
+        boolean output = false;
+        for (String s: validStatuses) {
+            output = output || status.equals(s);
+        }
+        return output;
+    }
 }
