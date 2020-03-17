e20bce86b805306ea8b180e2d4c6c3fe4068ba4b
chifa3 <foochifa@gmail.com>
2020-03-18 06:19:27 +0800

    Add Interview.java

---
+++ src/main/java/seedu/address/model/internship/interview/Interview.java
@@ -0,0 +1,33 @@
+package seedu.address.model.internship.interview;
+
+import java.time.LocalDate;
+
+import seedu.address.model.internship.Address;
+import seedu.address.model.internship.ApplicationDate;
+
+
+/**
+ * Represents an Interview in the Internship Diary.
+ * Interviews are always tagged to an internship application.
+ */
+public class Interview {
+    public final boolean isOnline;
+
+    private final ApplicationDate interviewDate;
+    private final Address interviewAddress;
+
+    public Interview(boolean isOnline, ApplicationDate interviewDate, Address interviewAddress) {
+        this.isOnline = isOnline;
+        this.interviewDate = interviewDate;
+        this.interviewAddress = interviewAddress;
+    }
+
+    public LocalDate getInterviewDate() {
+        return interviewDate.fullApplicationDate;
+    }
+
+    public Address getInterviewAddress() {
+        return interviewAddress;
+    }
+
+}
