aebbc42a7f3024fbf107cccc6639f75f65fd085a
chifa3 <foochifa@gmail.com>
2020-03-19 16:27:23 +0800

    Add InterviewBuilder.java and InterviewTest.java

---
+++ src/test/java/seedu/address/model/internship/interview/InterviewTest.java
@@ -0,0 +1,12 @@
+package seedu.address.model.internship.interview;
+
+import org.junit.jupiter.api.Test;
+
+public class InterviewTest {
+
+    @Test
+    public void equals() {
+
+        //same values
+    }
+}
