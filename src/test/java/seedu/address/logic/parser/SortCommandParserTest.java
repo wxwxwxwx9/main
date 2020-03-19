baa6cd5ba0f1284ec37164adf477c59bf21fb9ba
Ger Hean <gerhean@live.com>
2020-03-19 18:36:19 +0800

    Create SortCommandParserTest.java

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.comparator.CompanyComparator;
+
+public class SortCommandParserTest {
+
+    private SortCommandParser parser = new SortCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsSortCommand() {
+        // no leading and trailing whitespaces
+        SortCommand expectedSortCommand =
+                new SortCommand(new CompanyComparator());
+        assertParseSuccess(parser, PREFIX_COMPANY.toString(), expectedSortCommand);
+    }
+
+}
