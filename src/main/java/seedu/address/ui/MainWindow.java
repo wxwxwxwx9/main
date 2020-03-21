a8b36c57818fbb4e3f87cc8854538d18d860d138
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-03-21 14:56:34 +0800

    Revert "Revert "Revert "Revert "Add Statistics feature for application (with FXML)""""
    This reverts commit 3e12615b05f5864e9e730b1bf48dce33d096ada7.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -50,6 +50,9 @@ public class MainWindow extends UiPart<Stage> {
     @FXML
     private StackPane statusbarPlaceholder;
 
+    @FXML
+    private StackPane statisticsPlaceholder;
+
     public MainWindow(Stage primaryStage, Logic logic) {
         super(FXML, primaryStage);
 
@@ -116,6 +119,9 @@ public class MainWindow extends UiPart<Stage> {
 
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getInternshipDiaryFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
+
+        Statistics statistics = new Statistics(logic.getFilteredInternshipApplicationList());
+        statisticsPlaceholder.getChildren().add(statistics.getRoot());
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
