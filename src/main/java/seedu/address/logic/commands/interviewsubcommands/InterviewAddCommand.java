635e28e066526521504e9e99f920f01df0ba9195
chifa3 <foochifa@gmail.com>
2020-03-22 18:01:26 +0800

    implement execute method in InterviewAddCommand

--- src/main/java/seedu/address/logic/commands/interviewsubcommands/InterviewAddCommand.java
+++ src/main/java/seedu/address/logic/commands/interviewsubcommands/InterviewAddCommand.java
@@ -5,10 +5,15 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.InterviewCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.internship.InternshipApplication;
 import seedu.address.model.internship.interview.Interview;
 
+import static java.util.Objects.requireNonNull;
+
 public class InterviewAddCommand extends InterviewCommand {
-    public static final String MESSAGE_SUCCESS = "New Interview Added: %1$s";
+    public static final String MESSAGE_SUCCESS = "New Interview added: %1$s";
+    public static final String MESSAGE_DUPLICATE_INTERVIEW =
+            "This interview already exists in the internship application: %1$s";
 
     private final Index index;
     private final Interview toAdd;
@@ -20,7 +25,14 @@ public class InterviewAddCommand extends InterviewCommand {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        InternshipApplication internshipToModify = super.getInternshipApplication(model, index);
 
+        if (internshipToModify.hasInterview(toAdd)) {
+            throw new CommandException(String.format(MESSAGE_DUPLICATE_INTERVIEW, internshipToModify));
+        }
+
+        internshipToModify.addInterview(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 }
