61beb9775be097bc1bd0623255234a211dc4c131
chifa3 <foochifa@gmail.com>
2020-03-22 17:39:47 +0800

    Modify InterviewCommand.java into abstract class

--- src/main/java/seedu/address/logic/commands/InterviewCommand.java
+++ src/main/java/seedu/address/logic/commands/InterviewCommand.java
@@ -1,13 +1,10 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_IS_ONLINE;
 
-public class InterviewCommand extends Command {
+public abstract class InterviewCommand extends Command {
 
     public static final String COMMAND_WORD = "interview";
     public static final String MESSAGE_USAGE = COMMAND_WORD
@@ -15,15 +12,12 @@ public class InterviewCommand extends Command {
             + "by a command word to specify action to be taken.\n"
             + "Parameters: INDEX (must be a positive integer) COMMAND_CODE (must be either add, edit or delete)"
             + "[" + PREFIX_IS_ONLINE + "is it an online interview (true/false)] "
-            + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_DATE + "DATE] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS (optional if online interview] "
             + "Example: " + COMMAND_WORD + " 1 add "
             + PREFIX_IS_ONLINE + "false"
             + PREFIX_ADDRESS + "123 road"
             + PREFIX_DATE + "01 02 2020";
 
-    @Override
-    public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("test");
-    }
+
 }
