76c3917b6cac6ec9e7d98df95d0cc5dbe7618448
chifa3 <foochifa@gmail.com>
2020-03-22 16:23:00 +0800

    Add InterviewCode.java

---
+++ src/main/java/seedu/address/commons/core/interviewcode/InterviewCode.java
@@ -0,0 +1,18 @@
+package seedu.address.commons.core.interviewcode;
+
+/**
+ * Represents an enumeration of interview command code. This code should be either "add", "edit", or "delete",
+ * which will be used in interview command to manipulate interviews in an {@code Internship Application}.
+ *
+ * InterviewCode should come right after {@code Index} when inputting interview command.
+ */
+public enum InterviewCode {
+    ADD, EDIT, DELETE;
+
+    public static String MESSAGE_CONSTRAINTS = "Interview should have an add, edit or delete after index.";
+
+    public static boolean isValidCode(String code) {
+        String upperCaseCode = code.toUpperCase();
+        return upperCaseCode.equals("ADD") || upperCaseCode.equals("EDIT") || upperCaseCode.equals("DELETE");
+    }
+}
