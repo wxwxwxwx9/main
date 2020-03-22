bac3528bf9b08af4203534fd4b501b066ca82468
chifa3 <foochifa@gmail.com>
2020-03-22 21:41:29 +0800

    add typical interview indexes

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,4 +9,8 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_INTERNSHIP_APPLICATION = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_INTERNSHIP_APPLICATION = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_INTERNSHIP_APPLICATION = Index.fromOneBased(3);
+
+    public static final Index INDEX_FIRST_INTERVIEW = Index.fromOneBased(1);
+    public static final Index INDEX_SECOND_INTERVIEW = Index.fromOneBased(2);
+    public static final Index INDEX_THIRD_INTERVIEW = Index.fromOneBased(3);
 }
