2d82864e86bcd038f06be4f0cd60323065c438ac
FeliciaTay <E0323765@u.nus.edu>
2020-03-23 00:26:48 +0800

    Edit according to Checkstyle

--- src/main/java/seedu/address/model/InternshipDiary.java
+++ src/main/java/seedu/address/model/InternshipDiary.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 
 import javafx.collections.ObservableList;
-import seedu.address.logic.comparator.ApplicationDateThenInterviewDateComparator;
 import seedu.address.model.internship.InternshipApplication;
 import seedu.address.model.internship.UniqueInternshipApplicationList;
 
