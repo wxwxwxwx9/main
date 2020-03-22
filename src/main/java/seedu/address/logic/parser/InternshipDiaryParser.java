ebcaefadfd82e83696b5c9e8442ad7db354d00ff
chifa3 <foochifa@gmail.com>
2020-03-22 16:23:17 +0800

    Modify InternshipDiaryParser to accept Interview Command

--- src/main/java/seedu/address/logic/parser/InternshipDiaryParser.java
+++ src/main/java/seedu/address/logic/parser/InternshipDiaryParser.java
@@ -14,6 +14,7 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.InterviewCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -61,6 +62,9 @@ public class InternshipDiaryParser {
 
         case SortCommand.COMMAND_WORD:
             return new SortCommandParser().parse(arguments);
+
+        case InterviewCommand.COMMAND_WORD:
+            return new InterviewCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
