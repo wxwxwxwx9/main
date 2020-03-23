94fe69482f9bf39b954a902599d178f97f322237
FeliciaTay <E0323765@u.nus.edu>
2020-03-23 19:14:42 +0800

    Add tests for ReminderCommandTest

--- src/test/java/seedu/address/logic/commands/ReminderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ReminderCommandTest.java
@@ -2,12 +2,30 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.testutil.TypicalInternshipApplications.getTypicalInternshipDiary;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.ReminderCommand.MESSAGE_SUCCESS;
+import seedu.address.logic.comparator.ApplicationDateThenInterviewDateComparator;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class ReminderCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalInternshipDiary(), new UserPrefs());
+        expectedModel = new ModelManager(model.getInternshipDiary(), new UserPrefs());
+    }
+
     @Test
-    void equals() {
+    public void equals() {
         ReminderCommand firstReminder = new ReminderCommand();
 
         // same object -> returns true
@@ -18,5 +36,13 @@ class ReminderCommandTest {
 
         // null -> returns false
         assertNotEquals(null, firstReminder);
+    }
+
+    @Test
+    public void execute_afterReminder_showsFilteredList() {
+        expectedModel.updateFilteredInternshipApplicationList(new ApplicationDateThenInterviewDateComparator());
+        CommandResult expectedMessage = new CommandResult(MESSAGE_SUCCESS);
+        assertCommandSuccess(new ReminderCommand(), model, expectedMessage, expectedModel);
+
     }
 }
