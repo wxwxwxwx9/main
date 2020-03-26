ff50f15dacb8c1c5891eead6e81692a870a05881
Ger Hean <gerhean@live.com>
2020-03-26 20:11:14 +0800

    Add command history
    Up and down arrow to scroll

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,8 +1,13 @@
 package seedu.address.ui;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -17,6 +22,9 @@ public class CommandBox extends UiPart<Region> {
     private static final String FXML = "CommandBox.fxml";
 
     private final CommandExecutor commandExecutor;
+
+    private LinkedList<String> previousCommands = new LinkedList<>();
+    private ListIterator<String> previousCommandIterator;
 
     @FXML
     private TextField commandTextField;
@@ -34,10 +42,45 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private void handleCommandEntered() {
         try {
-            commandExecutor.execute(commandTextField.getText());
+            String text = commandTextField.getText();
+            commandExecutor.execute(text);
+            previousCommands.addFirst(text);
             commandTextField.setText("");
+            if (previousCommands.size() > 20) {
+                previousCommands.removeLast();
+            }
+            previousCommandIterator = null;
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
+        }
+    }
+
+    /**
+     * Handles any keyPressed pressed event.
+     */
+    @FXML
+    private void handleKeyPressed(KeyEvent ke) {
+        if (ke.getCode() == KeyCode.UP) {
+            if (previousCommandIterator == null) {
+                previousCommandIterator = previousCommands.listIterator(0);
+            }
+            if (previousCommandIterator.hasNext()) {
+                commandTextField.setText(previousCommandIterator.next());
+            }
+        } else if (ke.getCode() == KeyCode.DOWN) {
+            if (previousCommandIterator != null) {
+                if (previousCommandIterator.hasPrevious()) {
+                    previousCommandIterator.previous();
+                }
+                if (previousCommandIterator.hasPrevious()) {
+                    commandTextField.setText(previousCommandIterator.previous());
+                    previousCommandIterator.next();
+                } else {
+                    commandTextField.setText("");
+                }
+            }
+        } else if (ke.getCode() != KeyCode.LEFT && ke.getCode() != KeyCode.RIGHT) {
+            previousCommandIterator = null;
         }
     }
 
