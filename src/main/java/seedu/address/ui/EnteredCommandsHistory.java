6bd56e8609fab5e26b18dcbf46a10a460d4e59ec
Ger Hean <gerhean@live.com>
2020-03-26 20:44:58 +0800

    Add javadocs

--- src/main/java/seedu/address/ui/EnteredCommandsHistory.java
+++ src/main/java/seedu/address/ui/EnteredCommandsHistory.java
@@ -9,20 +9,45 @@ import java.util.ListIterator;
 public class EnteredCommandsHistory {
     private final LinkedList<String> commandsHistory = new LinkedList<>();
     private ListIterator<String> historyIterator;
+    private final int maxSize;
 
+    /**
+     * Uses the default {@code maxSize} of 20 for history.
+     */
+    public EnteredCommandsHistory() {
+        this.maxSize = 20;
+    }
 
+    /**
+     * Uses the given {@code maxSize} for the history.
+     */
+    public EnteredCommandsHistory(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    /**
+     * Adds string to history.
+     * @param command The string to be stored in the history.
+     */
     public void add(String command) {
         this.commandsHistory.addFirst(command);
-        if (this.commandsHistory.size() > 20) {
+        if (this.commandsHistory.size() > maxSize) {
             this.commandsHistory.removeLast();
         }
         this.historyIterator = null;
     }
 
+    /**
+     * Resets the history iterator.
+     */
     public void resetIterator() {
         this.historyIterator = null;
     }
 
+    /**
+     * Returns a command earlier in history.
+     * @return the string stored earlier in history.
+     */
     public String iterateNext() {
         if (this.historyIterator == null) {
             this.historyIterator = commandsHistory.listIterator(0);
@@ -33,6 +58,10 @@ public class EnteredCommandsHistory {
         return null;
     }
 
+    /**
+     * Returns a command later in history.
+     * @return the string stored later in history.
+     */
     public String iteratePrevious() {
         if (this.historyIterator != null) {
             if (this.historyIterator.hasPrevious()) {
