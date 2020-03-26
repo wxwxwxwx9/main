ffd34608e7f4898960a9b9a7e437b72624343d32
Ger Hean <gerhean@live.com>
2020-03-26 22:43:05 +0800

    add date time util tests

--- src/main/java/seedu/address/commons/util/DateTimeUtil.java
+++ src/main/java/seedu/address/commons/util/DateTimeUtil.java
@@ -1,5 +1,7 @@
 package seedu.address.commons.util;
 
+import static java.util.Objects.requireNonNull;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -42,22 +44,23 @@ public class DateTimeUtil {
     /**
      * Parse the input string as a LocalDateTime if possible.
      *
-     * @param dateTime String to be parsed.
+     * @param dateString String to be parsed.
      * @return The parsed string as a LocalDateTime.
      * @throws DateTimeParseException if string unable to be parsed.
      */
-    public static LocalDate parseDate(String dateTime) throws DateTimeParseException {
+    public static LocalDate parseDate(String dateString) throws DateTimeParseException {
+        requireNonNull(dateString);
         int currentYear = LocalDate.now().getYear();
-        dateTime = dateTime.replaceAll("[\\\\/\\-]+", " ");
+        dateString = dateString.replaceAll("[\\\\/\\- ]+", " ");
         for (int i = 0; i < 3; i++) {
             try {
-                return LocalDate.parse(dateTime, getDateTimeFormatter(i, currentYear));
+                return LocalDate.parse(dateString, getDateTimeFormatter(i, currentYear));
             } catch (DateTimeParseException e) {
                 // It's fine if DateTimeParseException is thrown now, it's only used to check format of date.
                 // DateTimeParseException will be thrown if all cases fail.
             }
         }
         // This throws a DateTimeParseException
-        return LocalDate.parse(dateTime, DateTimeFormatter.ofPattern("d/M/yyyy"));
+        return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("d/M/yyyy"));
     }
 }
---
+++ src/test/java/seedu/address/commons/util/DateTimeUtilTest.java
@@ -0,0 +1,61 @@
+package seedu.address.commons.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.internship.ApplicationDate;
+import seedu.address.model.internship.Company;
+
+public class DateTimeUtilTest {
+
+    @Test
+    public void parseDate_invalidDate_throwsDateTimeParseException() {
+        // null
+        String invalidApplicationDate0 = null;
+        // empty string
+        String invalidApplicationDate1 = "";
+        // random letters
+        String invalidApplicationDate2 = "asdfqawe";
+        // No numbers
+        String invalidApplicationDate3 = "/ / ";
+        // Invalid date-like string
+        String invalidApplicationDate4 = "12/13/2020";
+        // Bad date format
+        String invalidApplicationDate5 = "12^12^2020";
+        // Extra stuff
+        String invalidApplicationDate6 = "12/12/2020 haha";
+        // Too much numbers
+        String invalidApplicationDate7 = "12/12/12/2020";
+        assertThrows(NullPointerException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate0));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate1));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate2));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate3));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate4));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate5));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate6));
+        assertThrows(DateTimeException.class, () -> DateTimeUtil.parseDate(invalidApplicationDate7));
+
+    }
+
+    @Test
+    public void parseDate_validDate_returnsCorrectDate() {
+        int currentYear = LocalDate.now().getYear();
+
+        LocalDate date1 = LocalDate.of(2019, 3, 1);
+        LocalDate date2 = LocalDate.of(currentYear, 4, 2);
+
+        // parse correctly
+        assertEquals(DateTimeUtil.parseDate("1 3 2019"), date1);
+        assertEquals(DateTimeUtil.parseDate("01 03 2019"), date1);
+        assertEquals(DateTimeUtil.parseDate("1/3-2019"), date1);
+
+        assertEquals(DateTimeUtil.parseDate("2/4"), date2);
+    }
+}
