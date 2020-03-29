9d6c00d54af525b3b8c2a89c96621218b101fd9c
chifa3 <foochifa@gmail.com>
2020-03-29 19:50:06 +0800

    add Interview List and Card

---
+++ src/main/java/seedu/address/ui/InterviewCard.java
@@ -0,0 +1,39 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.internship.interview.Interview;
+
+public class InterviewCard extends UiPart<Region> {
+
+    public static final String FXML = "InterviewCard.fxml";
+
+    public final Interview interview;
+
+    @FXML
+    private HBox cardPane;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label date;
+
+    @FXML
+    private Label address;
+
+    public InterviewCard(Interview interview, int index) {
+        super(FXML);
+        this.interview = interview;
+        String title = "Interview " + index + ": ";
+        date.setText(interview.getDate().toString());
+        if (interview.isOnline) {
+            this.title.setText("ONLINE " + title);
+        } else {
+            this.title.setText("OFFLINE " + title);
+        }
+        address.setText(interview.getInterviewAddress().value);
+    }
+}
