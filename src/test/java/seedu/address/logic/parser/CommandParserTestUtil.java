5fdb8a52960cf615034c3fb60b28e3d9e5dcd88a
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2020-04-02 18:15:33 +0800

    Fix RemovalBasedCommandExecutionTypeParserTest

--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -32,6 +32,7 @@ public class CommandParserTestUtil {
             parser.parse(userInput);
             throw new AssertionError("The expected ParseException was not thrown.");
         } catch (ParseException pe) {
+            System.out.println(expectedMessage);
             assertEquals(expectedMessage, pe.getMessage());
         }
     }
