238c264d830b4a8c22e52b4a18c0b0e843455bb0
chifa3 <foochifa@gmail.com>
2020-04-07 15:06:30 +0800

    fix sample data to create IA with non-fixed size lists

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.model.util;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import seedu.address.model.InternshipDiary;
@@ -31,7 +32,7 @@ public class SampleDataUtil {
             new InternshipApplication(new Company("Google"), new Role("Software Developer"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), new Phone("87438807"),
                 new Email("google.recruit@example.com"), new ApplicationDate(LocalDate.now().minusDays(DAYS_TO_MINUS)),
-                new Priority(8), Status.APPLIED, false, Arrays.asList(sampleInterviews)),
+                new Priority(8), Status.APPLIED, false, new ArrayList<>(Arrays.asList(sampleInterviews))),
             new InternshipApplication(new Company("Facebook"), new Role("Software Engineer"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Phone("99272758"),
                 new Email("facebook.recruit@example.com"),
@@ -44,11 +45,11 @@ public class SampleDataUtil {
             new InternshipApplication(new Company("Shopee"), new Role("Data Science"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Phone("91031282"),
                 new Email("shopee.recruit@example.com"), new ApplicationDate(LocalDate.now()),
-                new Priority(10), Status.WISHLIST, false, Arrays.asList(sampleInterviews)),
+                new Priority(10), Status.WISHLIST, false, new ArrayList<>(Arrays.asList(sampleInterviews))),
             new InternshipApplication(new Company("DSTA"), new Role("Software Engineer"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Phone("92492021"),
                 new Email("dsta.recruit@example.com"), new ApplicationDate(LocalDate.now().minusDays(DAYS_TO_MINUS)),
-                new Priority(2), Status.REJECTED, Status.APPLIED, Arrays.asList(sampleInterviews)),
+                new Priority(2), Status.REJECTED, Status.APPLIED, new ArrayList<>(Arrays.asList(sampleInterviews))),
             new InternshipApplication(new Company("Singtel"), new Role("Software Engineer"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), new Phone("92624417"),
                 new Email("singtel.recruit@example.com"), new ApplicationDate(LocalDate.now().minusDays(DAYS_TO_MINUS)),
